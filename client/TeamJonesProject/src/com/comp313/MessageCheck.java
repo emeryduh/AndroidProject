c1e2bfd30dd39ab8a2c647d411e5ec93db5fb27e
OzgunKilit <ozgunkilit@gmail.com>
2014-09-24 13:31:55 -0400

    Added MessageCheck and MessageInformation classes
    -MessageCheck class checks and validates if the message is null, gets
    and sets the message.
    -MessageInformation class stores variables for username and message
    content

---
+++ client/TeamJonesProject/src/com/comp313/MessageCheck.java
@@ -0,0 +1,48 @@
+package com.comp313;
+
+import android.R;
+
+public class MessageCheck {
+	
+	private static MessageInformation[] messageInfo = null;
+	
+	
+	//Checks if message is null
+	public static MessageInformation checkMessage(String username)
+	{
+		MessageInformation messageContent = null;
+		if (messageInfo != null) 
+		{
+			for (int i = 0; i < messageInfo.length;) 
+			{
+				
+					messageContent = messageInfo[i];
+					break;
+								
+			}			
+		}		
+		return messageContent;
+	}
+	
+	public static MessageInformation getMessageInfo(MessageInfo[] messageInfo)
+	{
+		MessageInformation messageContent = null;
+		if (messageInfo != null) 
+		{
+			for (int i = 0; i < messageInfo.length;) 
+			{
+				
+					messageContent = messageInfo[i];
+					break;
+								
+			}			
+		}		
+		return messageContent;
+	}
+	
+	public static MessageInformation setMessageInfo(MessageInfo[] messageInfo)
+	{
+		MessageCheck.messageInfo = messageInfo;
+	}
+
+}
---
+++ client/TeamJonesProject/src/com/comp313/MessageInformation.java
@@ -0,0 +1,14 @@
+package com.comp313;
+
+import com.comp313.R.string;
+
+public class MessageInformation {
+	
+	public static  String USERNAME = "from";
+	public static  String MESSAGETEXT = "text";
+
+	
+	public String username;
+	public String messagetext;
+
+}
